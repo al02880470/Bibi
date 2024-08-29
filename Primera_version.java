a7182ff02ae9962abd2daf0faabbf3962ac098ff
al02880470 <al02880470@tecmilenio.mx>
2024-08-28 19:52:40 -0600

    Add files via upload

---
+++ Primera_version.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primera_version;
+
+/**
+ *
+ * @author arram
+ */
+public class Primera_version {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Esta es  la primera version");
+    }
+    
+}
